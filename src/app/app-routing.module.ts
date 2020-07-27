5839ea883eed6d4dcd72c10aff8e816809f44121
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2020-07-28 01:47:08 +0500

    update added routing or new compnents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroComponent } from "./hero/hero.component";
+import { StoryComponent } from "./story/story.component";
+import { DishesComponent } from "./dishes/dishes.component";
+import { from } from 'rxjs';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'hero',
+  //   component: HeroComponent
+  // },
+  // {
+  //   path: 'story',
+  //   component: StoryComponent
+  // },
+  // {
+  //   path: 'dishes',
+  //   component: DishesComponent
+  // }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
@@ -6,17 +7,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { StoryComponent } from './story/story.component';
+import { DishesComponent } from './dishes/dishes.component';
+import { MenuComponent } from './menu/menu.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HeroComponent,
+    StoryComponent,
+    DishesComponent,
+    MenuComponent,
+    BlogComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CarouselModule
+    CarouselModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
